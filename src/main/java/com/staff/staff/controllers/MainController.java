97ac121ef0fe97204b87c7a9fea725aaec1c5c09
Alex <ovskykrolik@gmail.com>
2020-06-16 15:47:55 +0300

    Initial commit

---
+++ src/main/java/com/staff/staff/controllers/MainController.java
@@ -0,0 +1,41 @@
+package com.staff.staff.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Main Page");
+        return "home";
+    }
+
+}
+
+
+
+
+
+
+
+
+/*
+* import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+}
+* */
