6a4ba1b0bac6a4e276098d1da5992c35967d200f
krolikovsky <ovskykrolik@gmail.com>
2020-06-28 14:00:46 +0300

    ...

--- src/main/java/com/staff/staff/controllers/LoginController.java
+++ src/main/java/com/staff/staff/controllers/LoginController.java
@@ -1,5 +1,8 @@
 package com.staff.staff.controllers;
 
+import com.staff.staff.models.Staff;
+import com.staff.staff.repo.StaffRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+    private final StaffRepo staffRepository;
+
+    public LoginController(StaffRepo staffRepository) {
+        this.staffRepository = staffRepository;
+    }
+
     @GetMapping("/login")
     public String login(Model model) {
-        model.addAttribute("title", "Login Page");
+        Iterable<Staff> staffs = staffRepository.findAll();
+        model.addAttribute("staffs", staffs);
         return "login";
 
     }
--- src/main/java/com/staff/staff/models/Staff.java
+++ src/main/java/com/staff/staff/models/Staff.java
@@ -1,4 +1,93 @@
 package com.staff.staff.models;
 
-public class staff {
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name, position;
+    private Date time;
+    private int totalSalary, everyDaySalary, everyWeekSalary, everyMonthSalary;
+    private boolean atWork;
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isAtWork() {
+        return atWork;
+    }
+
+    public void setAtWork(boolean atWork) {
+        this.atWork = atWork;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public int getTotalSalary() {
+        return totalSalary;
+    }
+
+    public void setTotalSalary(int totalSalary) {
+        this.totalSalary = totalSalary;
+    }
+
+    public int getEveryDaySalary() {
+        return everyDaySalary;
+    }
+
+    public void setEveryDaySalary(int everyDaySalary) {
+        this.everyDaySalary = everyDaySalary;
+    }
+
+    public int getEveryWeekSalary() {
+        return everyWeekSalary;
+    }
+
+    public void setEveryWeekSalary(int everyWeekSalary) {
+        this.everyWeekSalary = everyWeekSalary;
+    }
+
+    public int getEveryMonthSalary() {
+        return everyMonthSalary;
+    }
+
+    public void setEveryMonthSalary(int everyMonthSalary) {
+        this.everyMonthSalary = everyMonthSalary;
+    }
 }
